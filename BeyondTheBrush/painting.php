bde2b3c65d16c6cb9ebfeef1d41afa9c2e0e8662
Joshua Guinto <jguinto143@gmail.com>
2025-06-27 08:54:58 +0800

    ryle,joshua,risen

--- BeyondTheBrush/painting.php
+++ BeyondTheBrush/painting.php
@@ -1,5 +1,12 @@
+<?php
+include('connect.php');
+
+$query = "SELECT * FROM paintings";
+$result = $conn->query($query);
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -8,73 +15,38 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
 
   <div class="container py-5">
     <div class="row justify-content-center">
-      <div class="col-12 col-md-4 mb-4">
-        <div class="card h-100 text-center">
-          <img src="../img/art1.jpg" class="card-img-top" alt="Art 1">
-          <div class="card-body">
-            <h5 class="card-title">Spoliarium</h5>
-            <a href="Spoliarium.php" class="btn btn-primary btn-view">VIEW</a>
-          </div>
-        </div>
-      </div>
 
-      <div class="col-12 col-md-4 mb-4">
-        <div class="card h-100 text-center">
-          <img src="../img/art2.jpg" class="card-img-top" alt="Art 2">
-          <div class="card-body">
-            <h5 class="card-title">Granadean Arabesque</h5>
-            <a href="Granadean Arabesque.php" class="btn btn-primary btn-view">VIEW</a>
+      <?php
+      if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+      ?>
+          <div class="col-12 col-md-4 mb-4">
+            <div class="card h-100 text-center">
+              <img src="<?php echo $row['image_path']; ?>" class="card-img-top" alt="<?php echo $row['title']; ?>">
+              <div class="card-body">
+                <h5 class="card-title"><?php echo $row['title']; ?></h5>
+                <a href="<?php echo $row['link_page']; ?>" class="btn btn-primary btn-view">VIEW</a>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+      <?php
+        }
+      } else {
+        echo "<p>No paintings found.</p>";
+      }
+      ?>
 
-      <div class="col-12 col-md-4 mb-4">
-        <div class="card h-100 text-center">
-          <img src="../img/art3.jpg" class="card-img-top" alt="Art 3">
-          <div class="card-body">
-            <h5 class="card-title">Las Virgenes Cristianas Expuestas al Populacho</h5>
-            <a href="Las Virgenes Cristianas Expuestas al Populacho.php" class="btn btn-primary btn-view">VIEW</a>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-12 col-md-4 mb-4">
-        <div class="card h-100 text-center">
-          <img src="../img/art4.jpg" class="card-img-top" alt="Art 4">
-          <div class="card-body">
-            <h5 class="card-title">The Blood Compact</h5>
-            <a href="The Blood Compact.php" class="btn btn-primary btn-view">VIEW</a>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-12 col-md-4 mb-4">
-        <div class="card h-100 text-center">
-          <img src="../img/art5.jpg" class="card-img-top" alt="Art 5">
-          <div class="card-body">
-            <h5 class="card-title">Bayanihan</h5>
-            <a href="Bayanihan.php" class="btn btn-primary btn-view">VIEW</a>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-12 col-md-4 mb-4">
-        <div class="card h-100 text-center">
-          <img src="../img/art6.jpg" class="card-img-top" alt="Art 6">
-          <div class="card-body">
-            <h5 class="card-title">Planting Rice</h5>
-            <a href="Planting Rice.php" class="btn btn-primary btn-view">VIEW</a>
-          </div>
-        </div>
-      </div>
     </div>
   </div>
+
   <a href="index.php" class="btn btn-light back-fixed">Back</a>
-  
+
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
